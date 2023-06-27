38dbbb105e3ddefc60801e1d447d1a99215606bd
crepusculumx <crepusculumx@qq.com>
2023-06-27 11:01:14 +0800

    feat: switch theme
    https://ng.ant.design/docs/customize-theme/zh
    https://github.com/yangjunhan/nz-themes

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.less'],
 })
-export class WelcomeComponent {}
+export class WelcomeComponent implements OnInit {
+  radioValue = 'A';
+  panels = [
+    {
+      active: true,
+      name: 'This is panel header 1',
+      disabled: false,
+    },
+    {
+      active: false,
+      disabled: false,
+      name: 'This is panel header 2',
+    },
+    {
+      active: false,
+      disabled: true,
+      name: 'This is panel header 3',
+    },
+  ];
+
+  constructor() {
+    return;
+  }
+
+  ngOnInit(): void {
+    return;
+  }
+}
--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -3,9 +3,19 @@ import { NgModule } from '@angular/core';
 import { WelcomeRoutingModule } from './welcome-routing.module';
 
 import { WelcomeComponent } from './welcome.component';
+import { NzCollapseModule } from 'ng-zorro-antd/collapse';
+import { CommonModule } from '@angular/common';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [WelcomeRoutingModule],
+  imports: [
+    WelcomeRoutingModule,
+    CommonModule,
+    NzCollapseModule,
+    NzRadioModule,
+    FormsModule,
+  ],
   declarations: [WelcomeComponent],
   exports: [WelcomeComponent],
 })
