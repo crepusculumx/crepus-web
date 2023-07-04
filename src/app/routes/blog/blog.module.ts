52722a682d00f3958d31da5ab4b92fa509b820c9
crepusculumx <crepusculumx@qq.com>
2023-07-04 08:34:38 +0800

    feat: blog page header

--- src/app/routes/blog/blog.module.ts
+++ src/app/routes/blog/blog.module.ts
@@ -5,8 +5,10 @@ import { BlogRoutingModule } from './blog-routing.module';
 import { BlogComponent } from './blog.component';
 import { HtmlDocComponent } from './widgets/html-doc/html-doc.component';
 
+import { SharedModule } from '../../shared/shared.module';
+
 @NgModule({
   declarations: [BlogComponent, HtmlDocComponent],
-  imports: [CommonModule, BlogRoutingModule],
+  imports: [CommonModule, BlogRoutingModule, SharedModule],
 })
 export class BlogModule {}
