7fb528468eb436665c2bc7a3c50dcbd53efcec8a
crepusculumx <crepusculumx@qq.com>
2024-06-07 19:07:07 +0800

    feat(layout): Resizable sidebar.

--- src/app/layout/basic-layout/basic-layout.component.ts
+++ src/app/layout/basic-layout/basic-layout.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MenusComponent } from './widgets/menus/menus.component';
 import { ThemeService } from '../../services/theme.service';
 import { MenusService } from '../services/menus.service';
-import { BehaviorSubject, delay, Observable } from 'rxjs';
+import { delay, Observable } from 'rxjs';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { Menus } from '../interfaces/menu';
 import { UserHeaderComponent } from './widgets/user-header/user-header.component';
 import { NzResizableModule, NzResizeEvent } from 'ng-zorro-antd/resizable';
+import { toSignal } from '@angular/core/rxjs-interop';
 @Component({
   selector: 'app-basic-layout',
   standalone: true,
@@ -33,20 +34,21 @@ export class BasicLayoutComponent {
     public menuService: MenusService,
   ) {}
 
-  public isCollapsed$ = new BehaviorSubject<boolean>(false);
+  public isCollapsed = signal(false);
 
   //ExpressionChangedAfterItHasBeenCheckedError
   //如果子组件构造时立刻通知修改menus，则会出现在变更检测中子组件更改父组件的情形
   //通过delay(0)延迟到下一个周期变更检测周期
-  public menus$: Observable<Menus> = this.menuService.menus$.pipe(delay(0));
+  private menus$: Observable<Menus> = this.menuService.menus$.pipe(delay(0));
+  public menus = toSignal(this.menus$);
 
-  siderWidth = 300;
-  id = -1;
+  public siderWidth = signal(300);
+  private frameId = -1;
 
   onSideResize({ width }: NzResizeEvent): void {
-    cancelAnimationFrame(this.id);
-    this.id = requestAnimationFrame(() => {
-      this.siderWidth = width!;
+    cancelAnimationFrame(this.frameId);
+    this.frameId = requestAnimationFrame(() => {
+      this.siderWidth.set(width!);
     });
   }
 }
